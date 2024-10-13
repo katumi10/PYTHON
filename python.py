18a800c08bfecdee34c1ad3998bf7942c0903f53
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-10-13 03:19:45 +0000

    My first program

---
+++ python.py
@@ -0,0 +1,13 @@
+print("hello world")
+age = 20;
+
+print(age);
+
+#receiving input
+name= input("What is your name? ")
+print("Hello " + name)
+
+#type conversion
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print(age)
