48ffc2229fabb21e263952a433d2d684fa022e29
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-11-08 17:32:59 +0000

    commit

---
+++ var.py
@@ -0,0 +1,11 @@
+
+
+first_name = "John"
+fav_food = "Pizza"
+hobby = "Dancing"
+
+
+#the "f" means format
+print(f"Hello my name is {first_name}")
+print(f"I like {fav_food}")
+print(f"And i like {hobby}")
