a255ab7502528cdf7749abd339d78343b4e1dfa5
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-11-17 17:06:36 +0000

    madlibs game

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# madlibs : a game where you create a story
+#          by filling in blanks with random words
+
+adjective1 = input("Enter an adjective(description): ")
+noun = input("Enter an noun(person,place or thing): ")
+adjective2 = input("Enter an adjective(description): ")
+verb = input("Enter a verb ending with ing: ")
+
+
+print(f"I went to the {adjective1} market today")
+print(f"And i saw {noun}")
+print(f"{noun} was {verb} in the {adjective1} market")
+print(f" I was {adjective2} as i watched her ")
