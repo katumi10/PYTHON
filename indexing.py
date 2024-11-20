61046b53a6c2015d27bc3f4e0a4fd77b1df218d1
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-11-20 04:26:14 +0000

    commit

---
+++ indexing.py
@@ -0,0 +1,14 @@
+# Indexing = accessing elements of a sequence using [] (indexing operator)
+#             [start : end : step]
+
+
+num = "1234-5678-9012-3456"
+
+# print(num[0])
+# print(num[:4]) first four numbers
+# print(num[5:9]) 5 inclusive 9 exclusive #5678
+# print(num[-1]) last character of string
+
+# print(num[::2]) print every char with the step of 2
+
+# print(num[::-1]) will reverse the string 
